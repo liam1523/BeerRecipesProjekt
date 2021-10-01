67a356ad76aeace551e836f1950774683a6af0b8
Liam Michaelsen <10664@student.eadania.dk>
2021-10-01 10:53:29 +0200

    :)

---
+++ BeerRecipeAPI/Data/BeerRecipeAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BeerRecipesProjekt.Models;
+
+namespace BeerRecipeAPI.Data
+{
+    public class BeerRecipeAPIContext : DbContext
+    {
+        public BeerRecipeAPIContext (DbContextOptions<BeerRecipeAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BeerRecipesProjekt.Models.BeerRecipe> BeerRecipe { get; set; }
+    }
+}
---
+++ BeerRecipesAPI/GetDbData.cs
@@ -0,0 +1,32 @@
+﻿using BeerRecipesProjekt.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeerRecipesAPI
+{
+    public static class GetDbData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new BeerRecipesProjektContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<BeerRecipesProjektContext>>()))
+            {
+                if (context.BeerRecipe.Any())
+                {
+                    return;
+
+                }
+
+
+            }
+
+        }
+
+    }
+
+}
